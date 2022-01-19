ca5352a43a38e524953d34d720440bbfdec25d22
vCesar1mx <cesarvaz.e7@gmail.com>
2022-01-19 17:34:05 -0600

    add

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const path = require('path')
+const router = express.Router();
+
+router.get('/',function(req,res){
+    res.sendFile(path.join(__dirname+'/index.html'));
+})
+
+app.use('/',router);
+app.listen(process.env.port || 80);
+
+console.log('Corriendo en el puerto 80');
